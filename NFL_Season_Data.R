dc14df9659b447462dc041d9c2307c0998dd5a1c
bschiwal <brandon.schiwal.@gmail.com>
2021-05-04 16:53:21 -0500

    Update NFL_Season_Data.R
    Testing data. Added EPA per Play for passers with over 200 attempts

--- NFL_Season_Data.R
+++ NFL_Season_Data.R
@@ -4,3 +4,13 @@ library(tidyverse)
 
 seasons<- 2019:2020
 pbp<- nflfastR::load_pbp(seasons)
+
+
+pass <-pbp%>% filter(qb_dropback==1,play_type=="pass")
+passEPA <- pass%>% 
+  select(posteam,passer_player_name,qb_epa,pass_attempt)%>%
+  group_by(passer_player_name)%>%
+  summarize(qb_epa_per_play=mean(qb_epa, na.rm = TRUE),attempts=sum(pass_attempt))%>%
+  filter(attempts>=200)
+
+
