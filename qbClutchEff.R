1b381899235dffbe6a4b0eb4b84aed6742a8f61b
Brandon Schiwal <tundrabooking@gmail.com>
2024-12-31 15:58:03 -0600

    Updated Stats through end of week 16

--- qbClutchEff.R
+++ qbClutchEff.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(ggrepel)
 
 ###Load play by play data
-seasons<- 2023
+seasons<- 2024
 pbp<- nflfastR::load_pbp(seasons)
 colr<-teams_colors_logos%>% select(team_abbr,team_color)
 
@@ -15,14 +15,14 @@ passtrailingend <-pbp%>% filter(qb_dropback==1,
                                 play_type=="pass",
                                 score_differential<=0,
                                 score_differential>=-8,
-                                half_seconds_remaining<=120 | game_half=="Overtime")
+                                half_seconds_remaining<=240 | game_half=="Overtime")
 
 ###Filter and summarize dataset by passer 
 passEPAtrailend <- passtrailingend%>% 
   select(posteam,posteam,passer_player_name,qb_epa,pass_attempt,cpoe)%>%
   group_by(passer_player_name,posteam)%>%
   summarize(qb_epa_per_play=mean(qb_epa, na.rm = TRUE),attempts=sum(pass_attempt),cpoe=mean(cpoe,na.rm=TRUE))%>%
-  filter(attempts>=10)%>%
+  filter(attempts>=20)%>%
   inner_join(colr,by = c("posteam"= "team_abbr"))
 rm(colr,passtrailingend,pbp,seasons)
 ###Create Chart
@@ -32,8 +32,8 @@ qbclutch<-ggplot(passEPAtrailend, aes(cpoe, qb_epa_per_play))+
   geom_text_repel(aes(label=passer_player_name),force=2)+
   labs(x= "Completion % Over Expectation (CPOE)", 
        y= "QB EPA per Pass Thrown", 
-       title="Quarterback Clutch Efficency 2023", 
-       subtitle ="Dropback Passes in Final Two Minutes of Half or Overtime When Trailing by < 8 Points; Min 15 Attempt", 
+       title="Quarterback Clutch Efficency 2024", 
+       subtitle ="Dropback Passes in Final Four Minutes of Half or Overtime When Trailing by < 8 Points; Min 20 Attempt", 
        caption="Made by @bschiwal; Data from @nflfastR")+
   geom_hline(yintercept=0,color="red",linetype="dashed")+
   geom_vline(xintercept=0,color="red",linetype="dashed")+
